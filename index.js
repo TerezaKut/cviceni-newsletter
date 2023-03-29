5776a959b996f76aa60e995361596c2910186c76
TerezaKut <114390034+TerezaKut@users.noreply.github.com>
2023-03-29 17:39:50 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const formular = document.querySelector('form')
+const text = document.querySelector('input')
 
 const odebirat = (event) => {
 	event.preventDefault()
@@ -7,4 +8,19 @@ const odebirat = (event) => {
 	formular.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`
 }
 
-formular.addEventListener('submit', odebirat)
+const kontrola = () => {
+	console.log('slustilo se to')
+	const email = text.value;
+	console.log(email);
+
+	if (email === '' || (!email.includes('@'))){
+		text.classList.add('cerveny_ramecek');
+	
+	} else {
+		text.classList.remove('cerveny_ramecek');
+	}
+}
+
+formular.addEventListener('submit', odebirat);
+text.addEventListener('input', kontrola);
+text.addEventListener('focus', kontrola)
